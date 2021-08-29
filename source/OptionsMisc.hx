ee6f36f3b1db8826deddb4171148b05dbf617392
Juanen100 <65170591+Juanen100@users.noreply.github.com>
2021-08-29 17:41:10 +0200

    First Commit Lol

---
+++ source/OptionsMisc.hx
@@ -0,0 +1,131 @@
+package;
+
+import Controls.Control;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.input.keyboard.FlxKey;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import Controls.KeyboardScheme;
+
+class OptionsMisc extends MusicBeatState
+{
+	
+	var selector:FlxText;
+	var curSelected:Int = 0;
+
+	var controlsStrings:Array<String> = [];
+
+	private var grpControls:FlxTypedGroup<Alphabet>;
+
+	override function create()
+	{
+		var menuBG:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		controlsStrings = CoolUtil.coolTextFile(Paths.txt('controls'));
+		menuBG.color = 0xFFea71fd;
+		menuBG.setGraphicSize(Std.int(menuBG.width * 1.1));
+		menuBG.updateHitbox();
+		menuBG.screenCenter();
+		menuBG.antialiasing = true;
+		add(menuBG);
+
+		controlsStrings = CoolUtil.coolStringFile(
+			(FlxG.save.data.health ? 'Full Health On' : 'Full Health Off')); //+
+			//"\nBot Mode " + (!FlxG.save.data.botplay ? 'Off' : 'On'));
+		trace("Apperance Settings: " + controlsStrings);
+
+		
+		grpControls = new FlxTypedGroup<Alphabet>();
+		add(grpControls);
+
+		for (i in 0...controlsStrings.length)
+		{
+				var controlLabel:Alphabet = new Alphabet(0, (70 * i) + 30, controlsStrings[i], true, false);
+				controlLabel.isMenuItem = true;
+				controlLabel.targetY = i;
+				grpControls.add(controlLabel);
+			// DONT PUT X IN THE FIRST PARAMETER OF new ALPHABET() !!
+		}
+		 
+
+		super.create();
+
+		//openSubState(new OptionsSubState()); lol
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if(controls.BACK)
+			FlxG.switchState(new OptionsSelectState());
+		if (controls.UP_P)
+			changeSelection(-1);
+		if (controls.DOWN_P)
+			changeSelection(1);
+
+		if (controls.ACCEPT)
+			{
+				if (curSelected != 5)
+					grpControls.remove(grpControls.members[curSelected]);
+				switch(curSelected)
+				{
+					case 0:
+						FlxG.save.data.health = !FlxG.save.data.health;
+						var ctrl:Alphabet = new Alphabet(0, (70 * curSelected) + 30, (!FlxG.save.data.health ? 'Full Health Off' : 'Full Health On'), true, false);
+						ctrl.isMenuItem = true;
+						ctrl.targetY = curSelected - 1;
+						grpControls.add(ctrl);
+					/*
+					case 1:
+						FlxG.save.data.botplay = !FlxG.save.data.botplay;
+						var ctrl:Alphabet = new Alphabet(0, (70 * curSelected) + 30, "Bot Mode " + (!FlxG.save.data.botplay ? "off" : "on"), true, false);
+						ctrl.isMenuItem = true;
+						ctrl.targetY = curSelected - 4;
+						grpControls.add(ctrl);
+					*/
+				}
+			}
+		FlxG.save.flush();
+	}
+
+	function changeSelection(change:Int = 0)
+		{
+			#if !switch
+			// NGio.logEvent('Fresh');
+			#end
+			
+			FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+	
+			curSelected += change;
+	
+			if (curSelected < 0)
+				curSelected = grpControls.length - 1;
+			if (curSelected >= grpControls.length)
+				curSelected = 0;
+	
+			// selector.y = (70 * curSelected) + 30;
+	
+			var bullShit:Int = 0;
+	
+			for (item in grpControls.members)
+			{
+				item.targetY = bullShit - curSelected;
+				bullShit++;
+	
+				item.alpha = 0.6;
+				// item.setGraphicSize(Std.int(item.width * 0.8));
+	
+				if (item.targetY == 0)
+				{
+					item.alpha = 1;
+					// item.setGraphicSize(Std.int(item.width));
+				}
+			}
+		}
+}
---
+++ source/OptionsSelectState.hx
@@ -0,0 +1,312 @@
+package;
+
+import flixel.input.gamepad.FlxGamepad;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+
+
+#if windows
+import Discord.DiscordClient;
+#end
+
+using StringTools;
+
+class OptionsSelectState extends MusicBeatState
+{
+
+    //Copié el FreeplayState y lo adapté xd
+
+	var songs:Array<SongMetadataa> = [];
+
+	var selector:FlxText;
+	var curSelected:Int = 0;
+	var curDifficulty:Int = 1;
+
+	var scoreText:FlxText;
+	var comboText:FlxText;
+	var diffText:FlxText;
+	var lerpScore:Int = 0;
+	var intendedScore:Int = 0;
+	var combo:String = '';
+
+	private var grpSongs:FlxTypedGroup<Alphabet>;
+	private var curPlaying:Bool = false;
+
+    var leVanilla:Bool = false;
+    var leMinigame:Bool = false;
+
+	private var iconArray:Array<HealthIcon> = [];
+
+	override function create()
+	{
+	//	var initSonglist = CoolUtil.coolTextFile(Paths.txt('ye'));
+
+        if (!FlxG.sound.music.playing)
+        {
+            FlxG.sound.playMusic(Paths.music('freakyMenu'));
+        }
+
+	//	for (i in 0...initSonglist.length)
+	//	{
+	//		var data:Array<String> = initSonglist[i].split(':');
+	//		songs.push(new SongMetadataa(data[0], Std.parseInt(data[2]), data[1]));
+	//	}
+
+		 #if windows
+		 // Updating Discord Rich Presence
+		 DiscordClient.changePresence("In the Freeplay Menu", null);
+		 #end
+
+		var isDebug:Bool = false;
+
+		#if debug
+		isDebug = true;
+		#end
+
+		addWeek(['Gameplay'], 1, ['dad']);
+		addWeek(['Apperance'], 1, ['dad']);
+		addWeek(['Misc'], 1, ['dad']);
+		addWeek(['FPS Thing'], 1, ['dad']);
+
+		// LOAD MUSIC
+
+		// LOAD CHARACTERS
+
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuBGBlue'));
+		add(bg);
+
+		grpSongs = new FlxTypedGroup<Alphabet>();
+		add(grpSongs);
+
+		for (i in 0...songs.length)
+		{
+			var songText:Alphabet = new Alphabet(0, (70 * i) + 30, songs[i].songName, true, false);
+			songText.isMenuItem = true;
+			songText.targetY = i;
+			grpSongs.add(songText);
+
+			var icon:HealthIcon = new HealthIcon(songs[i].songCharacter);
+			icon.sprTracker = songText;
+
+			// using a FlxGroup is too much fuss!
+			iconArray.push(icon);
+			add(icon);
+
+			// songText.x += 40;
+			// DONT PUT X IN THE FIRST PARAMETER OF new ALPHABET() !!
+			// songText.screenCenter(X);
+		}
+
+		changeSelection();
+
+		// FlxG.sound.playMusic(Paths.music('title'), 0);
+		// FlxG.sound.music.fadeIn(2, 0, 0.8);
+		selector = new FlxText();
+
+		selector.size = 40;
+		selector.text = ">";
+		// add(selector);
+
+		// JUST DOIN THIS SHIT FOR TESTING!!!
+		/* 
+			var md:String = Markdown.markdownToHtml(Assets.getText('CHANGELOG.md'));
+
+			var texFel:TextField = new TextField();
+			texFel.width = FlxG.width;
+			texFel.height = FlxG.height;
+			// texFel.
+			texFel.htmlText = md;
+
+			FlxG.stage.addChild(texFel);
+
+			// scoreText.textField.htmlText = md;
+
+			trace(md);
+		 */
+
+		super.create();
+	}
+
+	public function addSong(songName:String, weekNum:Int, songCharacter:String)
+	{
+		songs.push(new SongMetadataa(songName, weekNum, songCharacter));
+	}
+
+	public function addWeek(songs:Array<String>, weekNum:Int, ?songCharacters:Array<String>)
+	{
+		if (songCharacters == null)
+			songCharacters = ['dad'];
+
+		var num:Int = 0;
+		for (song in songs)
+		{
+			addSong(song, weekNum, songCharacters[num]);
+
+			if (songCharacters.length != 1)
+				num++;
+		}
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.sound.music.volume < 0.7)
+		{
+			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
+		}
+
+		lerpScore = Math.floor(FlxMath.lerp(lerpScore, intendedScore, 0.4));
+
+		if (Math.abs(lerpScore - intendedScore) <= 10)
+			lerpScore = intendedScore;
+
+
+		var upP = FlxG.keys.justPressed.UP;
+		var downP = FlxG.keys.justPressed.DOWN;
+		var accepted = controls.ACCEPT;
+
+		var gamepad:FlxGamepad = FlxG.gamepads.lastActive;
+
+		if (gamepad != null)
+		{
+			if (gamepad.justPressed.DPAD_UP)
+			{
+				changeSelection(-1);
+			}
+			if (gamepad.justPressed.DPAD_DOWN)
+			{
+				changeSelection(1);
+			}
+		}
+
+		if (upP)
+		{
+			changeSelection(-1);
+		}
+		if (downP)
+		{
+			changeSelection(1);
+		}
+
+
+		if (controls.BACK)
+		{
+			FlxG.switchState(new MainMenuState());
+		}
+
+		if (accepted)
+		{
+			switch (songs[curSelected].songName) {
+				case 'Gameplay':
+					if(accepted)
+						{
+							trace("Geimpley");
+							FlxG.switchState(new OptionsMenu());
+						}
+				case 'Apperance':
+					if(accepted)
+						{
+							trace("Aperancelol");
+							FlxG.switchState(new OptionsApperance());
+						}
+				case 'Misc':
+					if(accepted)
+						{
+							trace("Misc Random lol");
+							FlxG.switchState(new OptionsMisc());
+						}
+				case 'FPS Thing':
+					if(accepted)
+					{
+						trace("Set FPS to S H I T");
+						FlxG.switchState(new OptionsFpsMenu());
+					}
+			}
+			
+			var songFormat = StringTools.replace(songs[curSelected].songName, " ", "-");
+			switch (songFormat) {
+				case 'Dad-Battle': songFormat = 'Dadbattle';
+				case 'Philly-Nice': songFormat = 'Philly';
+			}
+			
+			//trace(songs[curSelected].songName);
+
+			var poop:String = Highscore.formatSong(songFormat, curDifficulty);
+
+			//trace(poop);
+		}
+	}
+
+	function changeSelection(change:Int = 0)
+	{
+		#if !switch
+		// NGio.logEvent('Fresh');
+		#end
+
+		// NGio.logEvent('Fresh');
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+
+		curSelected += change;
+
+		if (curSelected < 0)
+			curSelected = songs.length - 1;
+		if (curSelected >= songs.length)
+			curSelected = 0;
+
+		// selector.y = (70 * curSelected) + 30;
+		
+		// adjusting the highscore song name to be compatible (changeSelection)
+		// would read original scores if we didn't change packages
+		var songHighscore = StringTools.replace(songs[curSelected].songName, " ", "-");
+		switch (songHighscore) {
+			case 'Dad-Battle': songHighscore = 'Dadbattle';
+			case 'Philly-Nice': songHighscore = 'Philly';
+		}
+
+		var bullShit:Int = 0;
+
+		for (i in 0...iconArray.length)
+		{
+			iconArray[i].alpha = 0;
+		}
+
+		iconArray[curSelected].alpha = 0;
+
+		for (item in grpSongs.members)
+		{
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			item.alpha = 0.6;
+			// item.setGraphicSize(Std.int(item.width * 0.8));
+
+			if (item.targetY == 0)
+			{
+				item.alpha = 1;
+				// item.setGraphicSize(Std.int(item.width));
+			}
+		}
+	}
+}
+
+class SongMetadataa
+{
+	public var songName:String = "";
+	public var week:Int = 0;
+	public var songCharacter:String = "";
+
+	public function new(song:String, week:Int, songCharacter:String)
+	{
+		this.songName = song;
+		this.week = week;
+		this.songCharacter = songCharacter;
+	}
+}
