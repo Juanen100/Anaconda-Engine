d90c5a62e5ab1bc924665619fac9719db59bdd1c
Juanen100 <65170591+Juanen100@users.noreply.github.com>
2021-09-15 10:34:03 +0200

    Fixes the number error on the Alphabet

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -1,16 +1,32 @@
 package;
 
-import lime.utils.Assets;
+import flixel.FlxG;
+import openfl.utils.Assets;
+import lime.utils.Assets as LimeAssets;
+import lime.utils.AssetLibrary;
+import lime.utils.AssetManifest;
 
 using StringTools;
 
 class CoolUtil
 {
-	public static var difficultyArray:Array<String> = ['EASY', "NORMAL", "HARD"];
+	// [Difficulty name, Chart file suffix]
+	public static var difficultyStuff:Array<Dynamic> = [
+		['Easy', '-easy'],
+		['Normal', ''],
+		['Hard', '-hard']
+	];
 
 	public static function difficultyString():String
 	{
-		return difficultyArray[PlayState.storyDifficulty];
+		return difficultyStuff[PlayState.storyDifficulty][0].toUpperCase();
+	}
+
+	public static function boundTo(value:Float, min:Float, max:Float):Float {
+		var newValue:Float = value;
+		if(newValue < min) newValue = min;
+		else if(newValue > max) newValue = max;
+		return newValue;
 	}
 
 	public static function coolTextFile(path:String):Array<String>
@@ -23,6 +39,16 @@ class CoolUtil
 		}
 
 		return daList;
+	}
+
+	public static function numberArray(max:Int, ?min = 0):Array<Int>
+	{
+		var dumbArray:Array<Int> = [];
+		for (i in min...max)
+		{
+			dumbArray.push(i);
+		}
+		return dumbArray;
 	}
 
 	public static function coolStringFile(path:String):Array<String>
@@ -38,13 +64,18 @@ class CoolUtil
 		}
 
 
-	public static function numberArray(max:Int, ?min = 0):Array<Int>
-	{
-		var dumbArray:Array<Int> = [];
-		for (i in min...max)
-		{
-			dumbArray.push(i);
+	//uhhhh does this even work at all? i'm starting to doubt
+	public static function precacheSound(sound:String, ?library:String = null):Void {
+		if(!Assets.cache.hasSound(Paths.sound(sound, library))) {
+			FlxG.sound.cache(Paths.sound(sound, library));
 		}
-		return dumbArray;
+	}
+
+	public static function epicOpenUrl(site:String) {
+		#if linux
+		Sys.command('/usr/bin/xdg-open', [site, "&"]);
+		#else
+		FlxG.openURL(site);
+		#end
 	}
 }
