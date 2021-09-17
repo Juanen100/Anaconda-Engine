3bc1fc04ddfdd3469e91f9fe1aa50917d977d84e
Juanen100 <65170591+Juanen100@users.noreply.github.com>
2021-09-17 20:35:22 +0200

    Update PlayStateConfig.hx

--- source/PlayStateConfig.hx
+++ source/PlayStateConfig.hx
@@ -1,5 +1,5 @@
 class PlayStateConfig
 {
-    //Just added botplay 'cause i dont know anything else lol
+    //Just added botplay 'cause i used to the pause menu
     public static var botPlay:Bool;
 }
