1d72c57a8a454b7a199fbb54eee22a4bbbd8f24a
Juanen100 <65170591+Juanen100@users.noreply.github.com>
2021-09-25 10:48:40 +0200

    The Options Menu and Options Stuff are now on the same .hx

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -2,7 +2,6 @@ package;
 
 import lime.math.ColorMatrix;
 import openfl.Lib;
-import Options;
 import Controls.Control;
 import flash.text.TextField;
 import flixel.FlxG;
@@ -56,7 +55,7 @@ class OptionsMenu extends MusicBeatState
 	{
 		var menuBG:FlxSprite = new FlxSprite().loadGraphic(Paths.image("main_menu/menuDesat"));
 
-		menuBG.color = 0xFF00D310;
+		menuBG.color = 0xFF5EAFA1;
 		menuBG.setGraphicSize(Std.int(menuBG.width * 1.1));
 		menuBG.updateHitbox();
 		menuBG.screenCenter();
@@ -240,5 +239,317 @@ class OptionsMenu extends MusicBeatState
 				// item.setGraphicSize(Std.int(item.width));
 			}
 		}
+	}
+}
+
+class OptionCatagory
+{
+	private var _options:Array<Option> = new Array<Option>();
+	public final function getOptions():Array<Option>
+	{
+		return _options;
+	}
+
+	public final function addOption(opt:Option)
+	{
+		_options.push(opt);
+	}
+
+	
+	public final function removeOption(opt:Option)
+	{
+		_options.remove(opt);
+	}
+
+	private var _name:String = "New Catagory";
+	public final function getName() {
+		return _name;
+	}
+
+	public function new (catName:String, options:Array<Option>)
+	{
+		_name = catName;
+		_options = options;
+	}
+}
+
+class Option
+{
+	public function new()
+	{
+		display = updateDisplay();
+	}
+	private var description:String = "";
+	private var display:String;
+	public final function getDisplay():String
+	{
+		return display;
+	}
+
+	public final function getDescription():String
+	{
+		return description;
+	}
+
+	// Returns whether the label is to be updated.
+	public function press():Bool { return throw "stub!"; }
+	private function updateDisplay():String { return throw "stub!"; }
+}
+
+class ControlOption extends Option
+{
+	public function new()
+	{
+		super();
+	}
+	
+	public override function press():Bool
+	{
+		trace("switch");
+		FlxG.switchState(new KeyBindMenu());
+		return false;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Keybinds";
+	}
+}
+
+class DownscrollOption extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+
+	public override function press():Bool
+	{
+		FlxG.save.data.downscroll = !FlxG.save.data.downscroll;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return FlxG.save.data.downscroll ? "Downscroll" : "Upscroll";
+	}
+}
+
+class InputOption extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+
+	public override function press():Bool
+	{
+		FlxG.save.data.newInput = !FlxG.save.data.newInput;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return FlxG.save.data.newInput ? "Kade Engine Input" : "Original Input";
+	}
+}
+
+class CpuStrums extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+
+	public override function press():Bool
+	{
+		FlxG.save.data.cpuStrums = !FlxG.save.data.cpuStrums;
+		
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return  FlxG.save.data.cpuStrums ? "Light CPU Strums" : "CPU Strums stay static";
+	}
+
+}
+
+class Colour extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.save.data.colour = !FlxG.save.data.colour;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return FlxG.save.data.colour ? 'Color bar by Character' : 'Color bar classic';
+	}
+}
+
+class Health extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.save.data.health = !FlxG.save.data.health;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return  !FlxG.save.data.health ? "Full Health Off" : "Full Health On";
+	} //FlxG.save.data.betterIcons ? "Better Icons On" : "Better Icons Off"
+}
+
+class BotPlay extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	
+	public override function press():Bool
+	{
+		FlxG.save.data.botplay = !FlxG.save.data.botplay;
+		trace('BotPlay : ' + FlxG.save.data.botplay);
+		display = updateDisplay();
+		return true;
+	}
+	
+	private override function updateDisplay():String
+		return "BotPlay " + (FlxG.save.data.botplay ? "on" : "off");
+}
+
+class Haxeflixel extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.save.data.skip = !FlxG.save.data.skip;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return  !FlxG.save.data.skip ? "Haxeflixel Intro On" : "Haxeflixel Intro Off";
+	}
+}
+
+class BetterIcon extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.save.data.betterIcons = !FlxG.save.data.betterIcons;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return  FlxG.save.data.betterIcons ? "Better Icons On" : "Better Icons Off";
+	}
+}
+
+class FlashingLightsOption extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.save.data.flashing = !FlxG.save.data.flashing;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Flashing Lights " + (!FlxG.save.data.flashing ? "off" : "on");
+	}
+}
+
+class Reset extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.save.data.resetButton = !FlxG.save.data.resetButton;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Reset Button " + (!FlxG.save.data.resetButton ? "off" : "on");
+	}
+}
+
+class ResetSettings extends Option
+{
+	var confirm:Bool = false;
+
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+
+	public override function press():Bool
+	{
+		if (!confirm)
+		{
+			confirm = true;
+			display = updateDisplay();
+			return true;
+		}
+		
+		TheData.resetSave();
+		TheData.saveLoad();
+		confirm = false;
+		trace('All settings have been reset');
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return confirm ? "Confirm Settings Reset" : "Reset Settings";
 	}
 }
