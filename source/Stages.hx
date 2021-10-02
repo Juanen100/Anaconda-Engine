c6d5a617ebe3a9edb6ca0d6e2764c342a704d1aa
Juanen100 <65170591+Juanen100@users.noreply.github.com>
2021-10-02 11:24:04 +0200

    Case 'stage' cuz yes

--- source/Stages.hx
+++ source/Stages.hx
@@ -371,6 +371,34 @@ class Stages
 							add(waveSpriteFG);
 						 */
 					}
+				case 'stage':
+					camZoom = 0.9;
+					curStage = 'stage';
+					var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('stageback'));
+					bg.antialiasing = FlxG.save.data.antialiasing;
+					bg.scrollFactor.set(0.9, 0.9);
+					bg.active = false;
+					swagBacks['bg'] = bg;
+                     toAdd.push(bg);
+
+					var stageFront:FlxSprite = new FlxSprite(-650, 600).loadGraphic(Paths.image('stagefront'));
+					stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
+					stageFront.updateHitbox();
+					stageFront.antialiasing = FlxG.save.data.antialiasing;
+					stageFront.scrollFactor.set(0.9, 0.9);
+					stageFront.active = false;
+					swagBacks['stageFront'] = stageFront;
+                     toAdd.push(stageFront);
+
+					var stageCurtains:FlxSprite = new FlxSprite(-500, -300).loadGraphic(Paths.image('stagecurtains'));
+					stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
+					stageCurtains.updateHitbox();
+					stageCurtains.antialiasing = FlxG.save.data.antialiasing;
+					stageCurtains.scrollFactor.set(1.3, 1.3);
+					stageCurtains.active = false;
+
+					swagBacks['stageCurtains'] = stageCurtains;
+                    toAdd.push(stageCurtains);
 				default:
 					{
 						camZoom = 0.9;
